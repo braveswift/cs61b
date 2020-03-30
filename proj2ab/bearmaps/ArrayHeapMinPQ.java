ae86b40c6a8d775e5173485a467b45d6b390f555
Yan Hong <hongyan968@gmail.com>
2020-03-30 10:43:42 -0700

    completed proj2a

---
+++ proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -0,0 +1,137 @@
+package bearmaps;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+import java.util.PriorityQueue;
+
+public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
+    private int size;
+    private HashMap<T, Integer> indexMap;
+    private ArrayList<Node> minHeapList;
+
+    private class Node {
+        private T item;
+        private double priority;
+
+        public Node(T item, double priority) {
+            this.item = item;
+            this.priority = priority;
+        }
+    }
+
+    public ArrayHeapMinPQ() {
+        size = 0;
+        indexMap = new HashMap<>();
+        minHeapList = new ArrayList<>();
+        minHeapList.add(null);
+    }
+
+    /* Adds an item with the given priority value. Throws an
+     * IllegalArgumentException if item is already present.
+     * You may assume that item is never null. */
+    @Override
+    public void add(T item, double priority) {
+        if (contains(item)) {
+            throw new IllegalArgumentException();
+        }
+        size = size + 1;
+        minHeapList.add(new Node(item, priority));
+        swim(size);
+    }
+
+
+    /* Returns true if the PQ contains the given item. */
+    @Override
+    public boolean contains(T item) {
+        return indexMap.containsKey(item);
+    }
+
+    /* Returns the minimum item. Throws NoSuchElementException if the PQ is empty. */
+    @Override
+    public T getSmallest() {
+        if (indexMap.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return minHeapList.get(1).item;
+    }
+
+    /* Removes and returns the minimum item. Throws NoSuchElementException if the PQ is empty. */
+    @Override
+    public T removeSmallest() {
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
+        T returnItem = minHeapList.get(1).item;
+        indexMap.remove(minHeapList.get(1).item);
+        Node rightMost = minHeapList.get(size);
+        minHeapList.set(1, rightMost);
+        minHeapList.remove(size);
+        size = size - 1;
+        sink(1);
+        return returnItem;
+    }
+
+
+    /* Returns the number of items in the PQ. */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /* Changes the priority of the given item. Throws NoSuchElementException if the item
+     * doesn't exist. */
+    @Override
+    public void changePriority(T item, double priority) {
+        if (!contains(item)) {
+            throw new NoSuchElementException();
+        }
+        int index = indexMap.get(item);
+        minHeapList.get(index).priority = priority;
+        swim(index);
+        sink(index);
+    }
+
+
+    private void sink(int k) {
+        while (k * 2 <= size) {
+            int j = k * 2;
+            if (j < size && smaller(j + 1, j)) {
+                j = j + 1;
+            }
+            if (smaller(j, k)) {
+                exchange(j, k);
+                k = j;
+            } else {
+                break;
+            }
+        }
+    }
+
+
+    private void swim(int k) {
+        if (!indexMap.containsKey(minHeapList.get(k).item)) {
+            indexMap.put(minHeapList.get(k).item, k);
+        }
+        while (k > 1 && smaller(k, k / 2)) {
+            exchange(k, k / 2);
+            k = k / 2;
+        }
+    }
+
+    private boolean smaller(int i, int j) {
+        return minHeapList.get(i).priority < minHeapList.get(j).priority;
+    }
+
+    private void exchange(int i, int j) {
+        Node nodei = minHeapList.get(i);
+        Node nodej = minHeapList.get(j);
+        minHeapList.set(i, nodej);
+        minHeapList.set(j, nodei);
+        indexMap.put(nodei.item, j);
+        indexMap.put(nodej.item, i);
+    }
+
+}
+
+
--- proj2ab/bearmaps/ArrayHeapMinPQTest.java
+++ proj2ab/bearmaps/ArrayHeapMinPQTest.java
@@ -1,7 +1,132 @@
 package bearmaps;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Stopwatch;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
 
 public class ArrayHeapMinPQTest {
+    /* it needs add, swim and getSmallest are correct */
+    @Test
+    public void testGetSmallest() {
+        ArrayHeapMinPQ<String> minpq = new ArrayHeapMinPQ<>();
+        minpq.add("priority5", 5);
+        minpq.add("priority3", 3);
+        minpq.add("priority2", 2);
+        assertEquals("priority2", minpq.getSmallest());
+    }
+
+    /* It assumes add is correct. */
+    @Test
+    public void testContains() {
+        ArrayHeapMinPQ<String> minpq = new ArrayHeapMinPQ<>();
+        minpq.add("priority5", 5);
+        minpq.add("priority3", 3);
+        minpq.add("priority2", 2);
+        assertTrue(minpq.contains("priority3"));
+        assertFalse(minpq.contains("priority10"));
+
+    }
+
+    /* It assumes add is correct. */
+    @Test
+    public void testRemoveSmallest() {
+        ArrayHeapMinPQ<String> minpq = new ArrayHeapMinPQ<>();
+        minpq.add("priority3", 3);
+        minpq.add("priority2", 2);
+        minpq.add("priority5", 5);
+        minpq.add("priority6", 6);
+        minpq.add("priority1", 1);
+        assertEquals("priority1", minpq.removeSmallest());
+        assertEquals("priority2", minpq.removeSmallest());
+        assertFalse(minpq.contains("priority1"));
+        assertFalse(minpq.contains("priority2"));
+    }
+
+    @Test
+    public void testRemoveException() {
+        ArrayHeapMinPQ<String> minpq = new ArrayHeapMinPQ<>();
+        minpq.add("priority3", 3);
+        minpq.add("priority2", 2);
+        minpq.removeSmallest();
+        minpq.removeSmallest();
+        minpq.removeSmallest();
+    }
+
+    /* It assumes add is correct. */
+    @Test
+    public void testSize() {
+        ArrayHeapMinPQ<String> minpq = new ArrayHeapMinPQ<>();
+        minpq.add("priority3", 3);
+        minpq.add("priority2", 2);
+        minpq.add("priority5", 5);
+        minpq.add("priority6", 6);
+        minpq.add("priority1", 1);
+        assertEquals(5, minpq.size());
+    }
+
+    @Test
+    public void testChangePriority() {
+        ArrayHeapMinPQ<String> minpq = new ArrayHeapMinPQ<>();
+        minpq.add("priority3", 3);
+        minpq.add("priority2", 2);
+        minpq.add("priority5", 5);
+        minpq.add("priority6", 6);
+        minpq.add("priority8", 8);
+        minpq.add("priority4", 4);
+        minpq.changePriority("priority2", 1);
+        assertEquals("priority2", minpq.getSmallest());
+        minpq.changePriority("priority2", 10);
+        assertEquals("priority3", minpq.getSmallest());
+
+    }
+
+    @Test
+    public void testWithNaive() {
+        ArrayHeapMinPQ<Integer> aMinpq = new ArrayHeapMinPQ<>();
+        NaiveMinPQ<Integer> nMinqp = new NaiveMinPQ<>();
+        for (int i = 50; i > 0; i--) {
+            aMinpq.add(i, i);
+            nMinqp.add(i, i);
+        }
+        for (int i = 0; i < 20; i++) {
+            int k = StdRandom.uniform(1, 50);
+            int p = StdRandom.uniform(50);
+            aMinpq.changePriority(k, p);
+            nMinqp.changePriority(k, p);
+        }
+        assertEquals(aMinpq.getSmallest(), nMinqp.getSmallest());
+
+    }
+
+    @Test
+    public void testTime() {
+        Stopwatch sw1 = new Stopwatch();
+        ArrayHeapMinPQ<Integer> aMinpq = new ArrayHeapMinPQ<>();
+        for (int i = 10000; i > 0; i--) {
+            aMinpq.add(i, i);
+        }
+        for (int i = 0; i < 5000; i++) {
+            int k = StdRandom.uniform(3000, 7000);
+            int p = StdRandom.uniform(500);
+            aMinpq.changePriority(k, p);
+        }
+        System.out.println("Total time elapsed for Arrayminpq: " + sw1.elapsedTime() +  " seconds.");
+
+        Stopwatch sw2 = new Stopwatch();
+        NaiveMinPQ<Integer> nMinpq = new NaiveMinPQ<>();
+        for (int i = 10000; i > 0; i--) {
+            nMinpq.add(i, i);
+        }
+        for (int i = 0; i < 5000; i++) {
+            int k = StdRandom.uniform(3000, 7000);
+            int p = StdRandom.uniform(500);
+            nMinpq.changePriority(k, p);
+        }
+        System.out.println("Total time elapsed for Naiveyminpq: " + sw2.elapsedTime() +  " seconds.");
+
+    }
+
 }
